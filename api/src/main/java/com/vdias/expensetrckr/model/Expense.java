6296e3b31ad29dfdae742c2dc92cc97cec27b5c6
irmao <v.fernandesdias@gmail.com>
2020-10-04 16:27:08 -0300

    @GeneratedValue on id; Moving README location

--- api/src/main/java/com/vdias/expensetrckr/model/Expense.java
+++ api/src/main/java/com/vdias/expensetrckr/model/Expense.java
@@ -1,6 +1,8 @@
 package com.vdias.expensetrckr.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.LocalDateTime;
 
@@ -10,6 +12,7 @@ import java.time.LocalDateTime;
 @Entity
 public class Expense {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private LocalDateTime date;
     private ExpenseType expenseType;
